88db41b97eb0c72ce29e42b3d74d95cb338c3371
chenking <C-coding@macbook-pro.local>
2018-02-01 09:16:34 +0800

    Initial Commit

---
+++ rjl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rjl
+//
+//  Created by C-coding on 2018/2/1.
+//  Copyright © 2018年 com.wwzc.rjl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
